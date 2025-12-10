2a43b9e7af7499532fcfc8cc8d3be0036f27c724
Jack Jin <1756404649@qq.com>
2025-12-10 15:44:12 +0800

    feat(array): implement solution to remove duplicates from sorted array
    - Added two-pointer technique to efficiently remove duplicates in-place
    - Maintained relative order of unique elements
    - Optimized algorithm to run in O(n) time complexity
    - Used minimal extra space with O(1) memory usage
    - Handled edge cases including empty arrays and single-element arrays
    - Updated LeetCode problem template with complete solution

---
+++ 26.删除有序数组中的重复项.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=java
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                ++j;
+            }
+        }
+        return j;
+    }
+}
+// @lc code=end
