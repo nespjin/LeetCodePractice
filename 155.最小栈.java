6fd485a57840469782ef503277b9edce10169b3d
Jack Jin <1756404649@qq.com>
2026-03-23 13:35:28 +0800

    feat: add minimum stack implementation
    Implement MinStack class with O(1) time complexity for push, pop,
    top, and getMin operations using auxiliary stack to track minimum values

---
+++ 155.最小栈.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=java
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(minStack.empty() ? val: Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// @lc code=end
